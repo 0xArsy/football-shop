188c5e516dc88c9ab1506884ca7ec563fe1adaa5
0xArsy <zarsy11000@gmail.com>
2025-09-10 04:45:43 +0700

    finishing 2

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         Product.objects.create(
             name="Bola Adidas Al Rihla",
             price=500000,
-            description="Bola resmi Piala Dunia FIFA 2022, kualitas premium.",
+            description="Bola resmi Piala Dunia FIFA 2022, dirancang dengan presisi tinggi dan material berkualitas premium untuk performa maksimal di lapangan.",
             thumbnail="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2024/2/15/779aa8d5-2c6b-436d-be61-dea88404c943.png",
             category="Bola",
             is_featured=True,
@@ -16,17 +16,17 @@ def show_main(request):
         Product.objects.create(
             name="Bola Nike Flight",
             price=450000,
-            description="Bola latihan dengan teknologi AerowSculpt untuk stabilitas.",
+            description="Bola latihan dengan teknologi AerowSculpt, memberikan stabilitas udara yang lebih baik dan kontrol optimal bagi pemain.",
             thumbnail="https://down-id.img.susercontent.com/file/id-11134207-7r98s-lztyd93khu3x25",
             category="Bola",
             is_featured=False,
-            quantity=8,
+            quantity=4,
             brand="Nike"
         )
         Product.objects.create(
             name="Bola Molten F5A",
             price=300000,
-            description="Bola standar FIFA cocok untuk kompetisi lokal.",
+            description="Bola standar resmi FIFA, cocok digunakan dalam kompetisi lokal maupun latihan intensif, dengan daya tahan tinggi.",
             thumbnail="https://alatolahraga.id/wp-content/uploads/2024/12/bola-Molten-5-F5A-1510-CB-1.webp",
             category="Bola",
             is_featured=False,
